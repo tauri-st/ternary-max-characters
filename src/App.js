2dd8816960ca69e992d64dd5cbbfb167828c40d5
tauri_s <tauristclaire@gmail.com>
2023-12-13 15:06:53 -0800

    Add onChange function called updateMessage linked to the textarea

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ export default function App() {
 
   const [message, setMessage] = useState("");
 
+  const updateMessage = () => {
+    setMessage();
+  };
+
   return (
     <div className="App">
       <div className="status-update">
         <h1>Super Short Status!</h1>
         <p>Tell us how you are doing in 60 characters or less!</p>
         <form>
-          <textarea placeholder="What's good?" value={message} />
+          <textarea placeholder="What's good?" value={message} onChange={updateMessage} />
           <button type="submit">Tell the world!</button>
         </form>
       </div>
