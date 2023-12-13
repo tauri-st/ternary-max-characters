0f4d8826b7b22ba56afd55bdcadfee312741be31
tauri_s <tauristclaire@gmail.com>
2023-12-13 15:08:44 -0800

    Add create state for successful message submission

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 export default function App() {
 
   const [message, setMessage] = useState("");
+  const [showSuccess, setShowSuccess] = useState(false);
 
   const updateMessage = () => {
     setMessage();
