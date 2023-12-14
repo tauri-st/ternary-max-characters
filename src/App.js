8bbd9d090fdd0398afd5a4bd7a2553e9508a23ff
tauri_s <tauristclaire@gmail.com>
2023-12-13 16:37:06 -0800

    Add showSuccess conditional and organize app div tags into it depending on whether submit is a success or not

--- src/App.js
+++ src/App.js
@@ -18,15 +18,19 @@ export default function App() {
 
   return (
     <div className="App">
-      <p className="message-success">Thanks for the update!</p>
-      <div className="status-update">
-        <h1>Super Short Status!</h1>
-        <p>Tell us how you are doing in 60 characters or less!</p>
-        <form onSubmit={handleSubmit}>
-          <textarea className={message.length > maxLength ? "too-long" : ""} placeholder="What's good?" value={message} onChange={updateMessage} />
-          <button type="submit" disabled={message.length > maxLength}>Tell the world!</button>
-        </form>
-      </div>
+      {
+        showSuccess ? 
+        <p className="message-success">Thanks for the update!</p>
+        :
+        <div className="status-update">
+          <h1>Super Short Status!</h1>
+          <p>Tell us how you are doing in 60 characters or less!</p>
+          <form onSubmit={handleSubmit}>
+            <textarea className={message.length > maxLength ? "too-long" : ""} placeholder="What's good?" value={message} onChange={updateMessage} />
+            <button type="submit" disabled={message.length > maxLength}>Tell the world!</button>
+          </form>
+        </div>
+      }
     </div>
   );
 }
